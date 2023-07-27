a5bbc5733136ff8f62fb8013264afbe55861c283
Yann de The <ydethe@gmail.com>
2023-07-27 20:43:35 +0000

    Removed data deletion on restart

--- moodle/start.sh
+++ moodle/start.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 sudo docker compose down
-sudo rm -rf mariadb_data  moodle_data  moodledata_data
+# sudo rm -rf mariadb_data  moodle_data  moodledata_data
 sudo docker compose up -d --remove-orphans
