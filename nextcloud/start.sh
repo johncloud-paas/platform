2a4f598625bcda74c8c9adb81e0320e6d82bae34
Yann de The <ydethe@gmail.com>
2023-08-21 07:43:40 +0000

    Adjusted nextcloud

--- nextcloud/start.sh
+++ nextcloud/start.sh
@@ -1,15 +1,4 @@
 #! /bin/sh
 
-
-sudo docker run \
---sig-proxy=false \
---name nextcloud-aio-mastercontainer \
---restart unless-stopped \
---publish 8081:8080 \
---env APACHE_PORT=11000 \
---env APACHE_IP_BINDING=127.0.0.1 \
---env NEXTCLOUD_DATADIR="/home/yann/.ncdata" \
---volume nextcloud_aio_mastercontainer:./docker-aio-config \
---volume /var/run/docker.sock:/var/run/docker.sock:ro \
-nextcloud/all-in-one:latest
-
+sudo docker compose down
+sudo docker compose up -d --remove-orphans
