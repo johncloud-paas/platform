7c56d50d5c69cac49a8eb1376771684b42d00e47
Yann de The <ydethe@gmail.com>
2023-08-21 07:32:20 +0000

    Adjusted Nextcloud conf

--- nextcloud/start.sh
+++ nextcloud/start.sh
@@ -4,12 +4,12 @@
 sudo docker run \
 --sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
---restart always \
+--restart unless-stopped \
 --publish 8081:8080 \
 --env APACHE_PORT=11000 \
---env APACHE_IP_BINDING=0.0.0.0 \
+--env APACHE_IP_BINDING=127.0.0.1 \
 --env NEXTCLOUD_DATADIR="/home/yann/.ncdata" \
---volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume nextcloud_aio_mastercontainer:./docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest
 
