380aed9b49ec47eebda0d653b1f41e7edb77bb8f
Yann de The <ydethe@gmail.com>
2023-08-23 14:09:26 +0000

    Added chown command on data dir

--- mattermost/start.sh
+++ mattermost/start.sh
@@ -1,4 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
+set -o allexport
+source .env set
++o allexport
+
+sudo chown -R 2000:2000 ${JOHNCLOUD_ROOT}/mattermost
 sudo docker compose -f docker-compose.yml -f docker-compose.without-nginx.yml down
 sudo docker compose -f docker-compose.yml -f docker-compose.without-nginx.yml up -d
