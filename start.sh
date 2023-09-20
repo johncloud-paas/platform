a48c044e1f3056aa8884136c2a580dbf9f4e468c
Yann de Thé <ydethe@gmail.com>
2023-09-20 13:02:55 +0000

    Using docker volumes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo docker stack rm traefik
+sudo docker stack rm platform
 env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack config --compose-file - 
-env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack deploy --compose-file - traefik
-sudo docker service logs traefik_traefik -f
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack deploy --compose-file - platform
+sudo docker service logs platform_traefik -f
