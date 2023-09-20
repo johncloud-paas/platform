bfe4e768788bc23c9d6501ee109be635fcb8c422
Yann de Thé <ydethe@gmail.com>
2023-09-20 08:44:20 +0000

    Portainer OK dans swarm

--- portainer/start.sh
+++ portainer/start.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-sudo docker compose down
-sudo docker compose up -d --remove-orphans
+sudo docker stack rm portainer
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack config --compose-file - 
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack deploy --compose-file - portainer
+sudo docker service logs portainer_app -f
