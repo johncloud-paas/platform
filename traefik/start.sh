25452c519ffdb0cb721a5bd85c87c0a7f75255ff
Yann de Thé <ydethe@gmail.com>
2023-09-20 08:36:37 +0000

    Swarm OK

--- traefik/start.sh
+++ traefik/start.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-sudo docker compose down
-sudo docker compose up -d --remove-orphans
+sudo docker stack rm traefik
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack config --compose-file - 
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack deploy --compose-file - traefik
+sudo docker service logs traefik_traefik -f
