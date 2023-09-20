a85aa8c02dc1df2edeeff924240715d8408c2d8d
Yann de Thé <ydethe@gmail.com>
2023-09-20 08:50:54 +0000

    Reorg

--- portainer/start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-sudo docker stack rm portainer
-env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack config --compose-file - 
-env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack deploy --compose-file - portainer
-sudo docker service logs portainer_app -f
