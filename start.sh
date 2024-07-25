fedddaa37262704adb297b735efd36b3d3a7043c
Yann de The <ydethe@gmail.com>
2024-07-25 09:58:55 +0200

    wip

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ git pull
 sudo docker compose down
 sudo docker compose pull
 ( cd tcp_ping && pdm build )
-sudo docker compose up -d --remove-orphans
+sudo docker compose up -d --build --remove-orphans
 sudo docker compose logs -f
