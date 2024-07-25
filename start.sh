1d5ee07c58c4f46995c1234fd84cf69457c8d46f
Yann de The <ydethe@gmail.com>
2024-07-25 11:03:29 +0200

    TCP router working

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ sudo docker compose down
 sudo docker compose pull
 ( cd tcp_ping && pdm build )
 sudo docker compose up -d --build --remove-orphans
-sudo docker compose logs -f
+# sudo docker compose logs -f
