90345de9fd04a5746fe7e6f836ceadd13af9faa5
Yann de The <ydethe@gmail.com>
2024-11-08 14:14:00 +0000

    Revert "WIP with poste.io"
    This reverts commit b023125b850c1b6d6d9250c3762803a130cf91e2.

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 #! /bin/sh
 
-# git pull
+git pull
 sudo docker compose down
 sudo docker compose pull
-sudo rm -rf /var/platform/posteio/*
-# sudo mkdir -p /var/platform/posteio/ssl/letsencrypt
-# sudo cp /var/platform/traefik/certs/mail.johntest.ovh/certificate.pem /var/platform/posteio/ssl/letsencrypt/ca.crt
-# sudo cp /var/platform/traefik/certs/mail.johntest.ovh/privatekey.pem /var/platform/posteio/ssl/letsencrypt/server.key
-# sudo chown mail:mail -R /var/platform/posteio
-# sudo rm -rf /var/platform/traefik/certs/mail.johntest.ovh
-# sudo rm -rf /var/platform/traefik/certs/posteio.johntest.ovh
 sudo docker compose up -d --remove-orphans
 sudo docker compose logs -f
