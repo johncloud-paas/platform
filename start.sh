1a91cd9ac88c2f5f689b188384ec4115e7a3d3a2
Yann de The <ydethe@gmail.com>
2025-03-08 20:53:11 +0100

    To be tested

--- start.sh
+++ start.sh
@@ -2,6 +2,11 @@
 
 git pull
 sudo docker compose down
+sudo rm -rf /home/yann/johncloud_data/bunkerweb/*
+sudo mkdir -p /home/yann/johncloud_data/bunkerweb/bw-storage
+sudo chown -R root:101 /home/yann/johncloud_data/bunkerweb/bw-storage
+sudo chmod -R 770 /home/yann/johncloud_data/bunkerweb/bw-storage
+# sudo cp ./syslog-ng.conf /home/yann/johncloud_data/bunkerweb/syslog-ng.conf
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans --force-recreate
-sudo docker compose logs -f
+sudo docker compose logs -f 
