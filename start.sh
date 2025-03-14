c2e4d530a386e58b41f22e3c66e8dc5473537d6b
Yann de The <ydethe@gmail.com>
2025-03-14 15:31:34 +0100

    Working with docker volume

--- start.sh
+++ start.sh
@@ -14,15 +14,21 @@ if [[ -z "${JOHNCLOUD_ROOT}" ]]; then
 fi
 
 git pull
+sudo docker compose pull
 sudo docker compose down
 if [[ "${ERASE_BW_FOLDERS}" = "yes" ]]; then
   echo "Erasing $JOHNCLOUD_ROOT/bunkerweb/*"
   sudo rm -rf $JOHNCLOUD_ROOT/bunkerweb/*
 fi
-sudo mkdir -p $JOHNCLOUD_ROOT/bunkerweb/bw-storage
-sudo chown -R root:101 $JOHNCLOUD_ROOT/bunkerweb/bw-storage
-sudo chmod -R 770 $JOHNCLOUD_ROOT/bunkerweb/bw-storage
+
+# sudo mkdir -p $JOHNCLOUD_ROOT/bunkerweb/bw-storage
+# sudo chown -R root:101 $JOHNCLOUD_ROOT/bunkerweb/bw-storage
+# sudo chmod -R 770 $JOHNCLOUD_ROOT/bunkerweb/bw-storage
+
+sudo mkdir -p $JOHNCLOUD_ROOT/bunkerweb/letsencrypt
+sudo chown -R root:101 $JOHNCLOUD_ROOT/bunkerweb/letsencrypt
+sudo chmod -R 770 $JOHNCLOUD_ROOT/bunkerweb/letsencrypt
+
 sudo cp ./syslog-ng.conf $JOHNCLOUD_ROOT/bunkerweb/syslog-ng.conf
-sudo docker compose pull
 sudo docker compose up -d --remove-orphans --force-recreate
 sudo docker compose logs -f 
