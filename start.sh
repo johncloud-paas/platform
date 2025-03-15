f3a41f50151d847b53dc8e3449e36de40a9704cc
Yann de The <ydethe@gmail.com>
2025-03-15 19:38:48 +0100

    Improved start script

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 git pull
 sudo docker compose down
 sudo docker compose pull
-sudo docker compose up -d --remove-orphans
+sudo docker compose up -d --remove-orphans --force-recreate
 sudo docker compose logs -f
