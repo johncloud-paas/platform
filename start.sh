fd7bcd6f555468c93a1f16d4874266414f68efc5
Yann de The <ydethe@gmail.com>
2025-03-20 17:23:01 +0100

    Changed steps order in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 git pull
-sudo docker compose down
 sudo docker compose pull
+sudo docker compose down
 sudo docker compose up -d --remove-orphans --force-recreate
 sudo docker compose logs -f
+
