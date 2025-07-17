9921969cea6fca1fe9c3d6ee4e7ef9b90e32d801
Yann de Thé <ydethe@gmail.com>
2025-07-17 13:57:56 +0000

    wip

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 git pull
-sudo docker compose pull
-sudo docker compose down
-sudo docker compose up -d --remove-orphans --force-recreate
-sudo docker compose logs -f
+docker compose pull
+docker compose down
+docker compose up -d --remove-orphans --force-recreate
+docker compose logs -f
 
