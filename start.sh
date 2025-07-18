b92bc288cd94d012ac7dcfaf76447ea8eacd7440
Yann de Thé <ydethe@gmail.com>
2025-07-18 09:14:23 +0000

    swarm working

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-# git pull
-# docker compose pull
+git pull
 env $(grep -v '^#' .env | xargs) envsubst < docker-compose.tpl > docker-compose.yml
 sudo docker stack deploy -c docker-compose.yml stackdemo --detach=false 
 sudo docker service logs -f stackdemo_traefik
