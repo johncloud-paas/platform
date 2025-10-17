98ef375e6825352b936bdc2150d411e22ee4e0e3
Yann de Thé <ydethe@gmail.com>
2025-10-17 07:55:53 +0000

    Removed crowdsec and tailscale

--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@ git pull
 sudo docker compose pull
 sudo docker compose down
 sudo docker compose up -d --remove-orphans --force-recreate
-sudo docker compose logs -f
-
+sudo docker compose logs -f traefik
