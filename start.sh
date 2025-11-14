036a469e6aa88efb03c7f194383bfa2f4f106f83
Yann de The <ydethe@gmail.com>
2025-11-14 14:32:57 +0100

    Using authentik for auth

--- start.sh
+++ start.sh
@@ -6,9 +6,10 @@ git pull
 sudo docker compose pull
 sudo docker compose down
 sudo mkdir -p $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/loki $JOHNCLOUD_ROOT/promtail $JOHNCLOUD_ROOT/prometheus
+sudo rm -rf $JOHNCLOUD_ROOT/loki/config.yml $JOHNCLOUD_ROOT/loki/config.yml
 sudo cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d
-sudo cp ./loki/config.yml $JOHNCLOUD_ROOT/loki
-sudo cp ./promtail/config.yml:$JOHNCLOUD_ROOT/promtail
-sudo cp ./prometheus/*:$JOHNCLOUD_ROOT/prometheus
+sudo cp ./loki/* $JOHNCLOUD_ROOT/loki
+sudo cp ./promtail/* $JOHNCLOUD_ROOT/promtail
+sudo cp ./prometheus/* $JOHNCLOUD_ROOT/prometheus
 sudo docker compose up -d --remove-orphans --force-recreate
 # sudo docker compose logs -f traefik
