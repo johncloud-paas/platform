c90f4b104ecb911acd7ff862c70c5f968930cf65
Yann de The <ydethe@gmail.com>
2025-11-28 13:40:31 +0100

    Added prometheus flush

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ git pull
 sudo docker compose pull
 sudo docker compose down
 sudo mkdir -p $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/loki $JOHNCLOUD_ROOT/promtail $JOHNCLOUD_ROOT/prometheus
-sudo rm -rf $JOHNCLOUD_ROOT/loki/config.yml $JOHNCLOUD_ROOT/loki/config.yml
+sudo rm -rf $JOHNCLOUD_ROOT/prometheus/db
+sudo rm -rf $JOHNCLOUD_ROOT/loki/config.yml
 sudo cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d
 sudo cp ./loki/* $JOHNCLOUD_ROOT/loki
 sudo cp ./promtail/* $JOHNCLOUD_ROOT/promtail
