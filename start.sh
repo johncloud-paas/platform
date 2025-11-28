a520eaa9cab1d75b8b9b7b4f1a391c99b96c03dc
Yann de The <ydethe@gmail.com>
2025-11-28 14:32:16 +0100

    Merged cadvisor and node_exporter in prometheus

--- start.sh
+++ start.sh
@@ -2,15 +2,21 @@
 
 set -a; source .env; set +a
 
+rm -rf GeoLite2-City.mmdb*
+wget https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
+gunzip -d GeoLite2-City.mmdb.gz
+
 git pull
 sudo docker compose pull
 sudo docker compose down
-sudo mkdir -p $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/loki $JOHNCLOUD_ROOT/promtail $JOHNCLOUD_ROOT/prometheus
+sudo mkdir -p $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/loki $JOHNCLOUD_ROOT/alloy/geoip $JOHNCLOUD_ROOT/prometheus
 sudo rm -rf $JOHNCLOUD_ROOT/prometheus/db
+sudo rm -rf $JOHNCLOUD_ROOT/alloy/data
 sudo rm -rf $JOHNCLOUD_ROOT/loki/config.yml
 sudo cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d
 sudo cp ./loki/* $JOHNCLOUD_ROOT/loki
-sudo cp ./promtail/* $JOHNCLOUD_ROOT/promtail
+sudo cp ./alloy/* $JOHNCLOUD_ROOT/alloy
+sudo cp ./GeoLite2-City.mmdb $JOHNCLOUD_ROOT/alloy/geoip
 sudo cp ./prometheus/* $JOHNCLOUD_ROOT/prometheus
 sudo docker compose up -d --remove-orphans --force-recreate
 sudo docker compose logs -f 
