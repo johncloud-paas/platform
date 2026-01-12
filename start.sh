102dd6524ae544a2faa145e3d59380b88b601147
Yann de The <ydethe@gmail.com>
2026-01-12 17:23:58 +0100

    Fixed speedtest measurements

--- start.sh
+++ start.sh
@@ -14,16 +14,17 @@ set -a; source .env; set +a
     sudo docker compose down &&
     # sudo docker network rm -f $TRAEFIK_NETWORK $PROXY_NETWORK $TAILSCALE_NETWORK &&
     # sudo docker network create -d bridge --subnet=$TRAEFIK_NET.0/24 $TRAEFIK_NETWORK &&
-    sudo docker network rm -f $PROXY_NETWORK $TAILSCALE_NETWORK &&
-    sudo docker network create -d bridge --subnet=$PROXY_NET.0/24 $PROXY_NETWORK &&
-    sudo docker network create -d bridge --subnet=$TAILSCALE_NET.0/24 $TAILSCALE_NETWORK &&
-    sudo mkdir -p $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/alloy/geoip $JOHNCLOUD_ROOT/alloy/data &&
-    sudo rm -rf $JOHNCLOUD_ROOT/alloy/data &&
+    # sudo docker network rm -f $PROXY_NETWORK $TAILSCALE_NETWORK &&
+    # sudo docker network create -d bridge --subnet=$PROXY_NET.0/24 $PROXY_NETWORK &&
+    # sudo docker network create -d bridge --subnet=$TAILSCALE_NET.0/24 $TAILSCALE_NETWORK &&
+    sudo mkdir -p $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/alloy/geoip $JOHNCLOUD_ROOT/alloy/data $JOHNCLOUD_ROOT/prometheus/db &&
     sudo cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d &&
     sudo cp ./alloy/* $JOHNCLOUD_ROOT/alloy &&
+    sudo cp ./prometheus/* $JOHNCLOUD_ROOT/prometheus &&
     sudo mv ./GeoLite2-City.mmdb $JOHNCLOUD_ROOT/alloy/geoip &&
     sudo docker compose up -d --remove-orphans &&
     sudo chmod 644 $JOHNCLOUD_ROOT/traefik/conf.d/* &&
     sudo chmod 755 $JOHNCLOUD_ROOT/traefik/plugins &&
+    sudo chown 1000:1000 $JOHNCLOUD_ROOT/prometheus/* &&
     sudo docker compose logs -f $1
 )
