856fd47f451771fba60929edb4baaa11df66e92b
Yann de The <ydethe@gmail.com>
2026-01-13 09:41:39 +0100

    Added Traefik logs dashboard

--- start.sh
+++ start.sh
@@ -17,14 +17,17 @@ set -a; source .env; set +a
     # sudo docker network rm -f $PROXY_NETWORK $TAILSCALE_NETWORK &&
     # sudo docker network create -d bridge --subnet=$PROXY_NET.0/24 $PROXY_NETWORK &&
     # sudo docker network create -d bridge --subnet=$TAILSCALE_NET.0/24 $TAILSCALE_NETWORK &&
-    sudo mkdir -p $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/alloy/geoip $JOHNCLOUD_ROOT/alloy/data $JOHNCLOUD_ROOT/prometheus/db &&
+    sudo mkdir -p $JOHNCLOUD_ROOT/traefik/agent/positions $JOHNCLOUD_ROOT/traefik/dashboard $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/alloy/geoip $JOHNCLOUD_ROOT/alloy/data $JOHNCLOUD_ROOT/prometheus/db &&
     sudo cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d &&
     sudo cp ./alloy/* $JOHNCLOUD_ROOT/alloy &&
     sudo cp ./prometheus/* $JOHNCLOUD_ROOT/prometheus &&
     sudo mv ./GeoLite2-City.mmdb $JOHNCLOUD_ROOT/alloy/geoip &&
     sudo docker compose up -d --remove-orphans &&
     sudo chmod 644 $JOHNCLOUD_ROOT/traefik/conf.d/* &&
-    sudo chmod 755 $JOHNCLOUD_ROOT/traefik/plugins &&
-    sudo chown 1000:1000 $JOHNCLOUD_ROOT/prometheus/* &&
+    sudo chmod 755 -R $JOHNCLOUD_ROOT/traefik/plugins &&
+    sudo chown 1000:1000 -R $JOHNCLOUD_ROOT/prometheus/* &&
+    sudo chown 0:0 -R $JOHNCLOUD_ROOT/dockhand/* &&
+    sudo chown 1001:1001 -R $JOHNCLOUD_ROOT/traefik/dashboard &&
+    sudo chown 1001:1001 -R  $JOHNCLOUD_ROOT/traefik/agent/positions &&
     sudo docker compose logs -f $1
 )
