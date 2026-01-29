ce59fa52c9b25356596153dd8b8bd042695005a4
Yann de The <ydethe@gmail.com>
2026-01-29 21:39:18 +0100

    Deployment ok

--- core/setup_before_up.sh
+++ core/setup_before_up.sh
@@ -6,6 +6,8 @@ cd $1
 set -a; source .env; set +a
 
 (
+    envsubst < pangolin/config.yml.template > pangolin/config.yml &&
+    envsubst < traefik/dynamic_config.yml.template > traefik/dynamic_config.yml &&
     mkdir -p $JOHNCLOUD_ROOT/traefik/agent/positions $JOHNCLOUD_ROOT/traefik/dashboard $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d &&
     cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d
 )
