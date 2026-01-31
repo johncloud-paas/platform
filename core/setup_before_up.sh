2d666ee3d2c398a36001f6010cd0c6a217fb899f
Yann de The <ydethe@gmail.com>
2026-01-31 16:57:59 +0100

    Added pangolin SSO

--- core/setup_before_up.sh
+++ core/setup_before_up.sh
@@ -8,7 +8,9 @@ set -a; source .env; set +a
 (
     envsubst < pangolin/config.yml.template > pangolin/config.yml &&
     envsubst < traefik/dynamic_config.yml.template > traefik/dynamic_config.yml &&
-    mkdir -p $JOHNCLOUD_ROOT/traefik/agent/positions $JOHNCLOUD_ROOT/traefik/dashboard $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d $JOHNCLOUD_ROOT/middleware-manager/config/traefik/rules $JOHNCLOUD_ROOT/middleware-manager/config/middleware-manager &&
+    envsubst < traefik/static_config.yml.template > traefik/static_config.yml &&
+    mkdir -p $JOHNCLOUD_ROOT/traefik/agent/positions $JOHNCLOUD_ROOT/traefik/dashboard $JOHNCLOUD_ROOT/traefik/plugins $JOHNCLOUD_ROOT/traefik/conf.d/rules $JOHNCLOUD_ROOT/middleware-manager/config/middleware-manager &&
     touch $JOHNCLOUD_ROOT/middleware-manager/config/middleware-manager/templates.yaml &&
-    cp ./traefik/* $JOHNCLOUD_ROOT/traefik/conf.d
+    cp ./traefik/static_config.yml $JOHNCLOUD_ROOT/traefik/conf.d/ &&
+    cp ./traefik/dynamic_config.yml $JOHNCLOUD_ROOT/traefik/conf.d/rules
 )
